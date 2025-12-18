412213b4806a3be47141b7506c80a23869842c91
Carlos Alfonzo <carliitosway@gmail.com>
2025-12-18 14:11:43 +0100

    "refine home layout, glass pills and navbar styling"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,38 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
-  return (
-    <nav className={styles.navbar}>
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? `${styles.link} ${styles.active}` : styles.link
-        }
-      >
-        Home
-      </NavLink>
+  const linkClass = ({ isActive }) =>
+    isActive ? `${styles.link} ${styles.active}` : styles.link;
 
-      <NavLink
-        to="/about"
-        className={({ isActive }) =>
-          isActive ? `${styles.link} ${styles.active}` : styles.link
-        }
-      >
-        About
-      </NavLink>
-    </nav>
+  return (
+    <header className={styles.navWrap}>
+      <nav className={styles.navbar}>
+        {/* LEFT */}
+        <div className={styles.left}>
+          <NavLink to="/" className={linkClass}>
+            Home
+          </NavLink>
+
+          <NavLink to="/countries" className={styles.cta}>
+            Start Exploring
+          </NavLink>
+        </div>
+
+        {/* CENTER */}
+        <div className={styles.center} aria-label="Brand">
+          <NavLink to="/" className={styles.brand}>
+            CityVerse
+          </NavLink>
+        </div>
+
+        {/* RIGHT */}
+        <div className={styles.right}>
+          <NavLink to="/about" className={linkClass}>
+            About
+          </NavLink>
+        </div>
+      </nav>
+    </header>
   );
 }
 
